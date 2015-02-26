bd326dc42057f7c2e55bf55836dd51ada4dda8bc
Flavio W. Brasil <fwbrasil@gmail.com>
2015-02-26 10:42:45 +0100

    now we have all types

--- src/main/scala/net/fwbrasil/bond/Validator.scala
+++ src/main/scala/net/fwbrasil/bond/Validator.scala
@@ -26,6 +26,5 @@ class Validator1[T, V[_]](f: (T, T) => Boolean) extends Validator[T] {
   def apply[U <: T](w: Witness.Aux[U]) =
     new Validator0[T, V[w.T]]((v: T) => f(w.value, v))
 
-  implicit def lift[A, I <: T, B, O <: I with V[B]](value: I with V[A]): O = 
-    macro Macros.lift[V, A, I, B, O]
+  implicit def lift[O](value: V[_]): O = macro Macros.lift[V, O]
 }
--- src/main/scala/net/fwbrasil/bond/typelevel/Macros.scala
+++ src/main/scala/net/fwbrasil/bond/typelevel/Macros.scala
@@ -1,30 +1,26 @@
 package net.fwbrasil.bond.typelevel
 
-import scala.reflect.macros.blackbox.Context
+import scala.reflect.macros.whitebox.Context
 import language.experimental.macros
 import scala.language.experimental.macros
 import scala.reflect.macros.blackbox
 import java.lang.reflect.Field
 import shapeless.SingletonTypeUtils
 
-class Macros(val c: blackbox.Context) extends SingletonTypeUtils {
-  import c.universe._
-
-  def lift[V[_], A, I, B, O](value: c.Expr[I with V[A]])(
-    implicit v: WeakTypeTag[V[_]],
-    a: WeakTypeTag[A],
-    i: WeakTypeTag[I],
-    b: WeakTypeTag[B],
-    o: WeakTypeTag[O]): c.Expr[O] = {
+object Macros {
+  def lift[V[_], O](c: Context)(value: c.Expr[Any])(
+    implicit v: c.WeakTypeTag[V[_]],
+    o: c.WeakTypeTag[O]): c.Expr[O] = {
+    import c.universe._
     //    val a = value.actualType.baseType(value.actualType.baseClasses(1))
     //    val w = weakTypeOf[T]
     //    val b = a.typeArgs.filter(_ => true)
     //    val ConstantType(Constant(x: String)) = a.typeArgs.head 
-//    c.warning(c.enclosingPosition, showRaw(a))
-//    c.warning(c.enclosingPosition, showRaw(u))
-//    c.warning(c.enclosingPosition, showRaw(b))
-    println(showRaw(a)) 
-    println(showRaw(value.actualType.baseType(value.actualType.baseClasses(1))))
+    //    c.warning(c.enclosingPosition, showRaw(a))
+    //    c.warning(c.enclosingPosition, showRaw(u))
+    //    c.warning(c.enclosingPosition, showRaw(b))
+    println(showRaw(value.actualType))
+    println(showRaw(weakTypeOf[O]))
 
     //    val q"${tpeString: String}" = tpeSelector
     //    val tpe =
@@ -34,26 +30,53 @@ class Macros(val c: blackbox.Context) extends SingletonTypeUtils {
     //    typeCarrier(tpe)
 
     reify {
-      value.splice.asInstanceOf[O]
+      null.asInstanceOf[O]
     }
   }
-
-  //  private def inspect(v: Any) = {
-  //    val cls = v.getClass
-  //    val f = fields(cls)
-  //    f.foreach(print)
-  ////    f.map(_.get(v)).foreach(print)
-  //  }
-  //  
-  //  private def fields(c: Class[_]): Array[Field] = {
-  //    c.getDeclaredFields ++ fields(c.getSuperclass)
-  //  }
-  //  
-  //  private def print(b: Any) = {
-  ////    val s = try b.toString
-  ////    catch {
-  ////      case e => "error"
-  ////    }
-  //    c.warning(c.enclosingPosition, "a")
-  //  }
 }
+
+//class Macros(val c: blackbox.Context) extends SingletonTypeUtils {
+//  import c.universe._
+//
+//  def lift[O: WeakTypeTag](value: c.Expr[Any]): c.Expr[O]= {
+//    //    val a = value.actualType.baseType(value.actualType.baseClasses(1))
+//    //    val w = weakTypeOf[T]
+//    //    val b = a.typeArgs.filter(_ => true)
+//    //    val ConstantType(Constant(x: String)) = a.typeArgs.head 
+//    //    c.warning(c.enclosingPosition, showRaw(a))
+//    //    c.warning(c.enclosingPosition, showRaw(u))
+//    //    c.warning(c.enclosingPosition, showRaw(b))
+//    println(showRaw(value.actualType))
+//    println(showRaw(weakTypeOf[O]))
+//
+//    //    val q"${tpeString: String}" = tpeSelector
+//    //    val tpe =
+//    //      parseLiteralType(tpeString)
+//    //        .getOrElse(c.abort(c.enclosingPosition, s"Malformed literal $tpeString"))
+//    //
+//    //    typeCarrier(tpe)
+//
+//    reify {
+//      null.asInstanceOf[O]
+//    }
+//  }
+//
+//  //  private def inspect(v: Any) = {
+//  //    val cls = v.getClass
+//  //    val f = fields(cls)
+//  //    f.foreach(print)
+//  ////    f.map(_.get(v)).foreach(print)
+//  //  }
+//  //  
+//  //  private def fields(c: Class[_]): Array[Field] = {
+//  //    c.getDeclaredFields ++ fields(c.getSuperclass)
+//  //  }
+//  //  
+//  //  private def print(b: Any) = {
+//  ////    val s = try b.toString
+//  ////    catch {
+//  ////      case e => "error"
+//  ////    }
+//  //    c.warning(c.enclosingPosition, "a")
+//  //  }
+//}
